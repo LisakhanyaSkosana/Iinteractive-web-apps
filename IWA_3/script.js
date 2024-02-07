e7cb56d06c1ee490fe2c9ea948608470a671e9e0
LisakhanyaSkosana <lindiwelisakhaya@gmail.com>
2024-02-07 18:52:05 +0200

    challenges 1- 3

---
+++ IWA_3/script.js
@@ -0,0 +1,19 @@
+// configuration.js
+
+const company = (c) ACME Inc.
+const year = 2022
+ 
+
+// scripts.js
+
+import {company } from 'configuration'
+import {year} from 'configuration'
+
+
+const message = ('© ' , 'company' , 'year')
+
+console.log (message0)
+
+
+document.querySelector('footer').innerText = message
+ 
