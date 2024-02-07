4fc45ae2ad2fc245705b72baed7eb77bf85e99cb
LisakhanyaSkosana <133130015+LisakhanyaSkosana@users.noreply.github.com>
2024-02-07 16:20:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// challenge 1//
+
+
+const user = 'John'
+
+/* This is the firstname of the user */
+
+
+const surname = 'Smith'
+
+/* This is the lastname of the user */
+
+
+console.log(user, surname)
+
+
+ // challnge 2 //
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console.log(date)
+
+
+// Challenge 3 //
+
+/** 
+It is important to show the following message in order to:
+*- to assure users
+*- to scare hackers
+*- to impress investors
+**/
+
+const warn = 'security' 
+
+/* It is important to let user know when they can close the page  */
+
+cnosole.log(warn)
+ 
+
+ 
+
