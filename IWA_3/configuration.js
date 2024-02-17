7bdc0545d2bbc519c8b72b10b3986d10a19fa65e
LisakhanyaSkosana <lindiwelisakhaya@gmail.com>
2024-02-17 11:33:04 +0200

    updated

---
+++ IWA_3/configuration.js
@@ -0,0 +1,7 @@
+// configuration.js
+
+const company = '(c) ACME Inc.'
+const year = '2022'
+ 
+export { comapny } 
+export { year }
--- IWA_3/script.js
+++ IWA_3/script.js
@@ -1,19 +1,56 @@
-// configuration.js
-
-const company = (c) ACME Inc.
-const year = 2022
- 
 
 // scripts.js
 
-import {company } from 'configuration'
-import {year} from 'configuration'
+import {company } from './configuration.js'
+import {year} from './configuration.js'
 
 
-const message = ('© ' , 'company' , 'year')
+const message = ('© ' + company + " " + year) // concating string variables//
 
-console.log (message0)
+console.log (message)
 
 
 document.querySelector('footer').innerText = message
  
+// nwabisa.js
+
+firstname = "Nwabisa"
+surname = "Gabe"
+ role = "CEO"
+
+var display = firstname + " " + surname + " " + role
+
+console.log(display)
+
+document.querySelector('#nwabisa').innerText = display
+ 
+
+// johannes.js
+
+var  firstname = "Johannes"
+var  surname = "Potgieter"
+var role = "Intern"
+
+var display = firstname + " " + surname + " " + role
+
+console.log(dispaly)
+
+document.querySelector('#johannes').innerText = display
+ 
+
+// alex.js
+
+const firstname = "Alex"
+const surname = "Naidoo"
+const role = "Head of Marketing"
+
+const display= firstname + " " + surname + " " + role 
+
+console.log(display)
+
+document.querySelector('#alex').innerText = display
+ 
+
+// scripts.js
+
+console.log('Roles:'('${nwabisa}. ${johanes}. ${alex}.')
