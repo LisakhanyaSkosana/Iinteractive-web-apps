514251e0f5575557e11f0a4b8fbc2eb7d27bab07
LisakhanyaSkosana <lindiwelisakhaya@gmail.com>
2024-02-20 22:13:23 +0200

    updated

---
+++ IWA_4/script.js
@@ -0,0 +1,37 @@
+const year: 2050;
+const status: "parent";
+const count = 9;
+
+year.months = "January"; "December"; "March"; "December"
+console.log(year.months)
+
+month.date = "April"; 1,27
+console.log(month.date)
+
+var = count + 4;
+count = 9;
+13
+
+if (status = "parent") {
+    console.log('June', 'Youth Day')
+    let count = count + 1
+}
+
+console.log('August', 'Women’s Day')
+console.log('September', 'Heritage Day')
+date = 'December'
+console.log(date, 'Day of Reconciliation')
+let count = count + 3
+
+if (status = "parent") {
+    console.log(date, 'Christmas Day')
+    let count = count + 1
+}
+
+console.log(date, 'Day of Goodwill')
+let count = count + 1
+}
+
+console.log('Your status is:', parent)
+console.log('The year is:', 2050)
+console.log('The total holidays is:', count)
