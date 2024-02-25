843512f3e625e8d7374f4c8d90d01cf961547383
LisakhanyaSkosana <lindiwelisakhaya@gmail.com>
2024-02-25 14:59:12 +0200

    updated

--- IWA_4/script.js
+++ IWA_4/script.js
@@ -1,37 +1,37 @@
-const year: 2050;
-const status: "parent";
-const count = 9;
+const year = 2050;
+const status = "student";
+let count = 0;
+let date;
 
-year.months = "January"; "December"; "March"; "December"
-console.log(year.months)
+if (year == 2050) {
+	console.log("January", "New Year’s Day")
+    console.log("March", 'Human Rights Day')
+    date = 'April'
+    console.log(date, 'Family Day')
+    console.log(date, 'Freedom Day')
+    count = count + 4
 
-month.date = "April"; 1,27
-console.log(month.date)
+    if (status == "student") {
+      console.log('June', 'Youth Day')
+      count = count + 1
+  }
 
-var = count + 4;
-count = 9;
-13
+    console.log('August', 'Women’s Day')
+    console.log('September', 'Heritage Day')
+    date = 'December'
+    console.log(date, 'Day of Reconciliation')
+    count = count + 3
 
-if (status = "parent") {
-    console.log('June', 'Youth Day')
-    let count = count + 1
+    if (status == "parent") {
+      console.log(date, 'Christmas Day')
+      count = count + 1
+  }
+
+    console.log(date, 'Day of Goodwill')
+    count = count + 1
 }
 
-console.log('August', 'Women’s Day')
-console.log('September', 'Heritage Day')
-date = 'December'
-console.log(date, 'Day of Reconciliation')
-let count = count + 3
 
-if (status = "parent") {
-    console.log(date, 'Christmas Day')
-    let count = count + 1
-}
-
-console.log(date, 'Day of Goodwill')
-let count = count + 1
-}
-
-console.log('Your status is:', parent)
-console.log('The year is:', 2050)
-console.log('The total holidays is:', count)
+console.log("Your status is:", student)
+console.log("The year is:", date)
+console.log("The total holidays is:", count)
