e5211099ac510cc8d8b87cc81d1b5ccd6bed8086
LisakhanyaSkosana <lindiwelisakhaya@gmail.com>
2024-03-08 19:20:23 +0200

    updated

--- IWA_6/script.js
+++ IWA_6/script.js
@@ -5,12 +5,32 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number === primaryPhone 
+const primaryValid = typeof number === primaryPhone
 const secondaryValid = typeof number == secondaryPhone
 
-
-
-
-
 console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:',secondaryValid )
+console.log('Secondary phone is valid numerical string:', secondaryValid)
+
+
+
+
+// Challenge 2 //
+
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if (hourOfDay && minuteOfDay !== null) 
+    if (hourOfDay == '00' && minuteOfDay == '00') {
+        const taxAsDecimal = tax / '100';
+        const startingAfterTax = salary * '1' - taxAsDecimal;
+        const balance = starting - transport - food - rent;
+    }
+
+    console.log(balance.toFixed(3))
